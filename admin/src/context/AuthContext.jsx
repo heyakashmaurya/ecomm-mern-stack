ee1a96a44ff3c0161d6e6ade6ca7bcc7a281089f
Akash M <akkikeraste@gmail.com>
2026-01-14 09:53:28 +0530

    Update server URL to production environment

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const authDataContext = createContext();
 
 const AuthContext = ({ children }) => {
 
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = " https://ecart-ei63.onrender.com"
     let value = {serverUrl};
 
   return (
